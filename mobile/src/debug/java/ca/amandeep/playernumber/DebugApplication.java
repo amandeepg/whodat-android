dcad4a78b9e99fefd4ba9784ca2baa165c2d7e19
Amandeep Grewal <me@amandeep.ca>
2016-12-02 03:58:04 -0800

    Properly implement Stetho and add OkHttp caching.

--- mobile/src/debug/java/ca/amandeep/playernumber/DebugApplication.java
+++ mobile/src/debug/java/ca/amandeep/playernumber/DebugApplication.java
@@ -1,11 +1,23 @@
 package ca.amandeep.playernumber;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+
 import com.facebook.stetho.Stetho;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import okhttp3.OkHttpClient;
 
 public class DebugApplication extends PlayerNumberApplication {
 
     public void onCreate() {
         super.onCreate();
         Stetho.initializeWithDefaults(this);
+    }
+
+    @Override
+    protected OkHttpClient.Builder createOkHttpClientBuilder(@NonNull Context context) {
+        return super.createOkHttpClientBuilder(context)
+                .addNetworkInterceptor(new StethoInterceptor());
     }
 }
--- mobile/src/main/java/ca/amandeep/playernumber/PlayerNumberApplication.java
+++ mobile/src/main/java/ca/amandeep/playernumber/PlayerNumberApplication.java
@@ -1,18 +1,24 @@
 package ca.amandeep.playernumber;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.squareup.moshi.Moshi;
 
 import ca.amandeep.playernumber.api.PlayerNumberService;
 import ca.amandeep.playernumber.models.AutoValueMoshiFactory;
 import ca.amandeep.playernumber.models.CalendarAdapter;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.moshi.MoshiConverterFactory;
 
 public class PlayerNumberApplication extends Application {
-    public static final String BASE_URL = "https://d1de3t8oqfn2fp.cloudfront.net";
+    private static final String TAG = "PlayerNumberApplication";
+    private static final String BASE_URL = "https://d1de3t8oqfn2fp.cloudfront.net";
 
     private static PlayerNumberService sService;
 
@@ -25,6 +31,8 @@ public class PlayerNumberApplication extends Application {
 
         final Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(createOkHttpClientBuilder(getApplicationContext())
+                        .build())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .addConverterFactory(MoshiConverterFactory.create(new Moshi.Builder()
                         .add(AutoValueMoshiFactory.create())
@@ -33,5 +41,17 @@ public class PlayerNumberApplication extends Application {
                 .build();
 
         sService = retrofit.create(PlayerNumberService.class);
+    }
+
+    protected OkHttpClient.Builder createOkHttpClientBuilder(@NonNull Context context) {
+        final OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        try {
+            final Cache responseCache = new Cache(context.getCacheDir(), 5 * 1024 * 1024);
+            builder.cache(responseCache);
+        } catch (Exception e) {
+            Log.d(TAG, "Unable to set http cache", e);
+        }
+
+        return builder;
     }
 }
